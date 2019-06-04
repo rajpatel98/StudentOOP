484c2da24491f6ce0077edb7c1c2e1db9d1c3820
rajpatel98 <51166091+rajpatel98@users.noreply.github.com>
2019-06-04 12:27:35 -0400

    Create pointers.cpp

---
+++ my_code/pointers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "pointers.h"
+
+using namespace std;
+
+void inc_by_value(int n) {
+}
+
+
+void inc_with_pointer(int* iptr) {
+}
+
+
+void inc_with_reference(int& n) {
+}
+
+
+Thing** create_array_of_things(int n) {
+    return nullptr;
+}
+
+void print_all_things(Thing** things, int n) {
+}
+
+void double_all_things(Thing** things, int n) {
+}
+
+void delete_all_things(Thing** things, int n) {
+}
+
+
+void assignTA(Student& s, Student& ta) {
+}
+
+void printTAs(vector<Student> students) {
+}
+
