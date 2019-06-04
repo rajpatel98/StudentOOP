4aaece3dc7cea79d10371f232ca803ff8f315914
Raj Patel <rajpatel98@hotmail.com>
2019-06-04 11:41:34 -0400

    update

--- my_code/basics.cpp
+++ my_code/basics.cpp
@@ -8,13 +8,17 @@ using namespace std;
 /*
  * increment arg passed by copy
  * */
-void inc_pass_copy(int j) {
+void inc_pass_copy(int j)
+{
+    j++;
 }
 
 /*
  * increment arg passed by reference
  * */
-void inc_pass_ref(int& j) {
+void inc_pass_ref(int& j)
+{
+    j++;
 }
 
 /*
@@ -23,61 +27,93 @@ void inc_pass_ref(int& j) {
  * 0 filled for you.
  * Use `push_back()` to fill the vector.
  * */
-void fill_vector(vector<int>& v, int n) {
+void fill_vector(vector<int>& v, int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        v.push_back(i);
+    }
 }
 
 /*
  * Print all elements in a vector. Use a ranged for!
  * */
-void print_vector(const vector<int>& v) {
+void print_vector(const vector<int>& v)
+{
+    for(int i : v)
+    {
+        cout << i << "  ";
+    }
+    cout << endl;
 }
 
 /*
  * Print a pair of coordinates.
  * Output should look like "x = 9, y = 2".
  * */
-void print_coords(coord pos) {
+void print_coords(coord pos)
+{
+    cout << "x = " << pos.x << ", y = " << pos.y << endl;
 }
 
 /*
  * Square an integer.
  * */
-int square(int n) {
-    return 100;
+int square(int n)
+{
+    return n*n;
 }
 
 /*
  * Square a double.
  * */
-double square(double d) {
-    return 6.25;
+double square(double d)
+{
+    return d*d;
 }
 
 /*
  * Get the absolute value of a number.
  * */
-int my_abs(int n) {
-    return 10;
+int my_abs(int n)
+{
+    if (n >= 0) return n;
+    return -n;
 }
 
 /*
  * Sum from 0 to n using a `while` loop.
  * */
-int sum(int n) {
-    return 45;
+int sum(int n)
+{
+    int sum = 0;
+    int i = 0;
+    while (i<n)
+    {
+        sum += i;
+        i++;
+    }
+    return sum;
 }
 
 /*
  * Sum from 0 to n using a `for` loop.
  * */
-int sum2(int n) {
-    return 45;
+int sum2(int n)
+{
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        sum+=i;
+    }
+    return sum;
 }
 
 /*
  * say() is built for you!
  * */
-void say(std::string s) {
+void say(std::string s)
+{
     cout << s << endl;
 }
 
@@ -85,6 +121,13 @@ void say(std::string s) {
 /*
  * Use a ranged for to pull a string apart into its bits.
  * */
-void pull_apart_string(std::string s) {
+void pull_apart_string(std::string s)
+{
+    for (char c : s)
+    {
+        cout << c << " ";
+    }
+    cout << endl;
 }
+
 
