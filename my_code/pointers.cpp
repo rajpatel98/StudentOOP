149b764f829bd7b8d8b8f3efdb1a42118ed32f17
Raj Patel <rajpatel98@hotmail.com>
2019-06-04 12:51:06 -0400

    update

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -3,19 +3,31 @@
 
 using namespace std;
 
-void inc_by_value(int n) {
+void inc_by_value(int n)
+{
+    n++;
 }
 
 
-void inc_with_pointer(int* iptr) {
+void inc_with_pointer(int* iptr)
+{
+    (*iptr)++;
 }
 
 
-void inc_with_reference(int& n) {
+void inc_with_reference(int& n)
+{
+    n++;
 }
 
 
-Thing** create_array_of_things(int n) {
+Thing** create_array_of_things(int n)
+{
+    
+    for(int i = 0; i < n; i++)
+    {
+        
+    }
     return nullptr;
 }
 
