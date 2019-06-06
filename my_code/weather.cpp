2a7469f106c8421aa3259943d13afa524945d1d7
rajpatel98 <51166091+rajpatel98@users.noreply.github.com>
2019-06-06 11:26:49 -0400

    Create weather.cpp

---
+++ my_code/weather.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "weather.h"
+
+using namespace std;
+
+/*
+ * A constructor for weather class.
+ * */
+Weather::Weather(std::string nm, GPS loc) :
+    station_nm(nm), my_loc(loc) {
+}
+
+
+string Weather::get_name() {
+    return station_nm;
+}
+
