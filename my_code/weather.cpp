ed3a95ecdfdb628568adce9124d097d2b8b6e7dc
Raj Patel <rajpatel98@hotmail.com>
2019-06-06 11:36:39 -0400

    update

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -4,15 +4,32 @@
 
 using namespace std;
 
-/*
- * A constructor for weather class.
- * */
-Weather::Weather(std::string nm, GPS loc) :
-    station_nm(nm), my_loc(loc) {
+
+ostream& operator<<(ostream& os, const GPS& gps)
+{
+    
+    return os;
 }
 
 
-string Weather::get_name() {
+/*
+ * A constructor for weather class.
+ * */
+Weather::Weather(string nm, GPS loc): station_nm(nm), my_loc(loc){}
+
+string Weather::get_name() const
+{
     return station_nm;
+}
+
+int Weather::get_rating() const
+{
+    return rating;
+
+}
+
+void Weather::set_rating(int new_rating)
+{
+    rating = new_rating;
 }
 
