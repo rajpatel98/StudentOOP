f17268cf1b27df49acc13c73b037fad1610b0ca7
rajpatel98 <51166091+rajpatel98@users.noreply.github.com>
2019-06-07 12:12:49 -0400

    Rename date.cpp to date2.cpp

---
+++ my_code/date2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Date::Date(int d, int m, int y): day(d), month(m), year(y){}
+
+ostream& operator<<(std::ostream& os, const Date& date)
+{
+    os << date.day << "/" << date.month << "/" << date.year << " ";
+    return os;
+}
