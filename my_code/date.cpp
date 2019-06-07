cd3c0d76f1e435acc00eb63647d104fbb884d2ec
rajpatel98 <51166091+rajpatel98@users.noreply.github.com>
2019-06-07 12:12:03 -0400

    Create date.cpp

---
+++ my_code/date.cpp
@@ -0,0 +1,15 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <string>
+class Date {
+    friend std::ostream& operator<<(std::ostream& os, const Date& date);
+public:
+    Date(int d, int m, int y);
+private:
+    int day;
+    int month;
+    int year;
+};
+
+#endif
