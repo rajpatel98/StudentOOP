1e941839082d1820e4fd9e7538b3a37ec4c95ff8
Raj Patel <rajpatel98@hotmail.com>
2019-06-12 12:11:58 -0400

    update

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -5,6 +5,42 @@
 
 using namespace std;
 
+Image::Image(int w, int h, std::string flnm)
+: width(w), height(h)
+{
+    filename = flnm;
+    image_buf = new char[image_sz()];
+}
+
+// copy constructor:
+Image::Image(const Image& img2) {
+}
+
+Image::~Image() {
+}
+
+Image& Image::operator=(const Image& img2) {
+}
+
+int Image::image_sz() {
+    return width * height;
+}
+
+
+void Image::copy_fields(const Image& img2) {
+}
+
+
+/*
+ * Setting `display() = 0` here makes this an abstract
+ * class that can't be implemented.
+ * */
+string Image::display(std::string s) {
+    return "Displaying image " + s;
+}
+
+
+
 
 ostream& operator<<(ostream& os, const GPS& gps)
 {
