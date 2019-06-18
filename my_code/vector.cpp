ce8cc587ae638a8db5992b0900e7bcedc26bc828
Raj Patel <rajpatel98@hotmail.com>
2019-06-18 11:53:39 -0400

    update

--- my_code/vector.cpp
+++ my_code/vector.cpp
@@ -5,28 +5,47 @@ using namespace std;
 
 void print_vector(const MyVec& v)
 {
-    
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
 }
 
 
 MyVec::MyVec()
 {
-    
+    data = new int[DEF_CAPACITY];
+    sz = 0;
+    capacity = DEF_CAPACITY;
 }
 
 MyVec::MyVec(const MyVec& v2)
 {
-    
+    data = new int[v2.capacity];
+    sz = v2.sz;
+    capacity = v2.capacity;
+    for (int i = 0; i < sz; i++)
+    {
+        data[i] = v2[i];
+    }
 }
 
 MyVec::~MyVec()
 {
-    
+    delete[] data;
 }
 
 MyVec& MyVec::operator=(const MyVec& v2)
 {
-    
+    delete[] data;
+    sz = v2.sz;
+    capacity = v2.capacity;
+    data = new int[v2.capacity];
+    for (int i = 0; i < sz; i++)
+    {
+        data[i] = v2[i];
+    }
+    return *this;
 }
 
 
@@ -36,6 +55,11 @@ MyVec& MyVec::operator=(const MyVec& v2)
  * */
 bool operator==(MyVec& v1, MyVec& v2)
 {
+    if (v1.size() != v2.size()) return false;
+    for (int i = 0; i < v1.size(); i++)
+    {
+        if (v1[i] != v2[i]) return false;
+    }
     return true;
 }
 
@@ -44,8 +68,19 @@ bool operator==(MyVec& v1, MyVec& v2)
  * */
 void MyVec::push_back(int val)
 {
-    
+    if (sz == capacity)
+    {
+        int* old_data = data;
+        data = new int[capacity * 2];
+        for (int i = 0; i < sz; i++) {
+            data[i] = old_data[i];
+        }
+        capacity *= 2;
+        delete [] old_data;
+    }
+    data[sz++] = val;
 }
+
 
 /*
  * this [] is for reading items from the MyVec:
@@ -53,7 +88,7 @@ void MyVec::push_back(int val)
  * */
 int MyVec::operator[](int i) const
 {
-    return 0;
+    return data[i];
 }
 
 /*
@@ -62,6 +97,5 @@ int MyVec::operator[](int i) const
  * */
 int& MyVec::operator[](int i)
 {
-    int j = 0;
-    return j;
+    return *(data+i);
 }
