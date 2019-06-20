0596a58b15d3aea9a000d897e598a693c8460934
Raj Patel <rajpatel98@hotmail.com>
2019-06-20 11:38:44 -0400

    update

--- my_code/llist.cpp
+++ my_code/llist.cpp
@@ -9,6 +9,8 @@ using namespace std;
  * Output a Node pointer.
  * */
 ostream& operator<<(ostream& os, const Node* nd) {
+    if (!nd) return os << "null";
+    os << "Data: " << nd->data << " -> ";
     return os;
 }
 
@@ -17,6 +19,14 @@ ostream& operator<<(ostream& os, const Node* nd) {
  * Add a node to the end of a list.
  * */
 void add_at_end(Node*& head, int d) {
+    if (!head)
+    {
+        head = new Node(d, nullptr);
+    }
+    else
+    {
+        add_at_end(head->next, d);
+    }
 }
 
 
@@ -24,19 +34,27 @@ void add_at_end(Node*& head, int d) {
  * Given the head of a list, print the whole thing.
  * Let's do this recursively!
  * */
-void print_list(ostream& os, const Node* curr) {
+void print_list(ostream& os, const Node* curr)
+{
+    cout << curr;
+    if (curr) print_list(os, curr->next);
 }
 
 /*
  * Add a node at the front of a linked list.
  * */
-void add_at_front(Node*& head, int d) {
+void add_at_front(Node*& head, int d)
+{
+    head = new Node(d, head);
 }
 
 
 /*
  * Get the last node of a list.
  * */
-Node* last(Node* head) {
-    return nullptr;
+Node* last(Node* head)
+{
+    if (!head->next) return head;
+    if (head->next) last(head->next);
+    
 }
