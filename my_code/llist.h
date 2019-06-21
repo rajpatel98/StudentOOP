1fa2ecd187fad4ea16f68007c8a5cd837c64ba4d
rajpatel98 <51166091+rajpatel98@users.noreply.github.com>
2019-06-21 11:24:58 -0400

    Update llist.h

--- my_code/llist.h
+++ my_code/llist.h
@@ -22,15 +22,35 @@ void print_list(std::ostream& os, const Node* curr);
 Node* last(Node* head);
 void add_at_end(Node*& head, int d);
 void add_at_front(Node*& head, int d);
-bool del_tail(Node*& curr);
-
 
 /*
  * These are coming:
-bool del_head(Node*& head);
-bool del_tail(Node** prev_next);
-Node* duplicate(Node* head);
-Node* reverse(Node* curr, Node* prev=nullptr);
 */
+ 
+/*
+ * Delete the first node and attach head to the 2nd node:
+ * */
+bool del_head(Node*& head);
+
+/*
+ * Delete the last node and set prev->next to nullptr:
+ * */
+bool del_tail(Node*& head);
+
+/*
+ * Duplicate the entire list -- you must not share memory!
+ * */
+Node* duplicate(Node* head);
+
+/*
+ * Reverse the list: return a brand new list with everything reversed.
+ * */
+Node* reverse(Node* curr, Node* new_next=nullptr);
+
+/*
+ * Join two lists: tack list 2 onto the end of list 1:
+ * Use existing memory.
+ * */
+Node* join(Node*& list1, Node* list2);
 
 #endif
