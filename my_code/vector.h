988e69c99a4dd4d49e051f559a4851257d5a9ac5
rajpatel98 <51166091+rajpatel98@users.noreply.github.com>
2019-06-25 11:10:56 -0400

    Update vector.h

--- my_code/vector.h
+++ my_code/vector.h
@@ -1,10 +1,37 @@
 
 const int DEF_CAPACITY = 10;
+const int CAPACITY_MULT = 2;
 
-class MyVec
-{
+class MyVec {
     public:
+        class Iterator {
+        /*
+         * This class supports ranged for loops.
+         * It includes:
+         * Iterator(int*)
+         * operator++
+         * operator*
+         * operator!=
+         * */
+            friend bool operator!=(Iterator& rhs, Iterator& lhs) {
+                return true;
+            }
+
+        public:
+            Iterator(int* ip) : iptr(ip) {}
+
+            Iterator& operator++() {
+                return *this;
+            }
+
+            int operator*() { return 0; }
+
+        private:
+            int* iptr;
+    };
+
         MyVec();
+        MyVec(int sz, int val=0);
 
         // copy control:
         MyVec(const MyVec& v2);
@@ -15,7 +42,11 @@ class MyVec
         int size() const { return sz; }
         int operator[](int i) const;
         int& operator[](int i);
+        Iterator begin() const;
+        Iterator end() const;
+
     private:
+        void copy(const MyVec& v2);
         int* data;
         int sz;
         int capacity;
