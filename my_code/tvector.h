56859ab091075149c2110607a8816d1d423677d5
Raj Patel <rajpatel98@hotmail.com>
2019-06-26 12:16:45 -0400

    update

--- my_code/tvector.h
+++ my_code/tvector.h
@@ -144,6 +144,11 @@ void print_vector(const MyVec<DATA>& v) {
 template <typename DATA>
 bool operator==(MyVec<DATA>& v1, MyVec<DATA>& v2)
 {
+    if (v1.size() != v2.size()) return false;
+    for (int i = 0; i < v1.size(); i++)
+    {
+        if (v1[i] != v2[i]) return false;
+    }
     return true;
 }
 
