c55b9b2a89ad66e87fdd1b7414dfd13f9169cc83
Raj Patel <rajpatel98@hotmail.com>
2019-06-26 11:24:14 -0400

    update

--- my_code/tvector.h
+++ my_code/tvector.h
@@ -1,1 +1,63 @@
 
+const int DEF_CAPACITY = 10;
+const int CAPACITY_MULT = 2;
+
+class MyVec {
+public:
+    class Iterator {
+        /*
+         * This class supports ranged for loops.
+         * It includes:
+         * Iterator(int*)
+         * operator++
+         * operator*
+         * operator!=
+         * */
+        friend bool operator!=(Iterator& rhs, Iterator& lhs)
+        {
+            return (rhs.iptr != lhs.iptr);
+        }
+        
+    public:
+        Iterator(int* ip) : iptr(ip) {}
+        
+        
+        
+        Iterator& operator++()
+        {
+            iptr++;
+            return *this;
+        }
+        
+        int operator*() { return *iptr; }
+        
+    private:
+        int* iptr;
+    };
+    
+    MyVec();
+    MyVec(int sz, int val=0);
+    
+    // copy control:
+    MyVec(const MyVec& v2);
+    ~MyVec();
+    MyVec& operator=(const MyVec& v2);
+    
+    void push_back(int val);
+    int size() const { return sz; }
+    int operator[](int i) const;
+    int& operator[](int i);
+    Iterator begin() const;
+    Iterator end() const;
+    
+private:
+    void copy(const MyVec& v2);
+    int* data;
+    int sz;
+    int capacity;
+};
+
+
+void print_vector(const MyVec& v);
+
+bool operator==(MyVec& v1, MyVec& v2);
