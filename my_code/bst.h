b2e1679298b7ea34fa3d4deea15fab4078e80f0c
rajpatel98 <51166091+rajpatel98@users.noreply.github.com>
2019-06-28 10:57:50 -0400

    Update bst.h

--- my_code/bst.h
+++ my_code/bst.h
@@ -1,34 +1,72 @@
 #include <iostream>
 
+void indent(int level) {
+    // indent more at each level:
+    for (int i = 0; i < level; i++) std::cout << "  ";
+}
+
 template <typename T>
 class Bst {
-    friend void print_bst(const Bst<T>& bst);
+    friend void print_bst(const Bst<T>& bst, int level=0) {
+        indent(level);
+        level++;
+
+        std::cout << "data: " << bst.data << std::endl;
+        indent(level + 1);
+        std::cout << "left: ";
+   
+        if (!bst.left) std::cout << "nullptr\n";
+        else {
+            std::cout << "\n";
+            indent(level);
+            print_bst(*bst.left, level);
+        }
+        indent(level + 1);
+        std::cout << "right: ";
+        if (!bst.right) std::cout << "nullptr\n";
+        else {
+            std::cout << "\n";
+            indent(level);
+            print_bst(*bst.right, level);
+        }
+        std::cout << std::endl;
+    }
 
     public:
-        Bst(T d, Bst* l=nullptr, Bst* r=nullptr)
-            : data(d), left(l), right(r) {}
+        Bst(T d, Bst* p=nullptr, Bst* l=nullptr, Bst* r=nullptr)
+            : data(d), parent(p), left(l), right(r) {}
+
+        Bst<T>* insert(const T d) {
+        }
+
+        T get_val() const {
+            return data;
+        }
+
     private:
         T data;
+        Bst* parent;
         Bst* left;
         Bst* right;
 };
 
 
-template <typename T>
-void print_bst(const Bst<T>& bst) {
-    std::cout << "data: " << bst.data << "; left: ";
-
-    if (!bst.left) std::cout << "nullptr ";
-    else {
-        std::cout << "\t\n";
-        print_bst(bst.left);
-    }
-    std::cout << "; right: ";
-    if (!bst.right) std::cout << "nullptr ";
-    else {
-        std::cout << "\t\n";
-        print_bst(bst.right);
-    }
-    std::cout << std::endl;
-}
+//template <typename T>
+//void print_bst(const Bst<T>& bst) {
+//void print_bst(Bst<int> bst) {
+//    std::cout << "data: " << bst.data << "; left: ";
+//
+//    if (!bst.left) std::cout << "nullptr ";
+//    else {
+//        std::cout << "\t\n";
+//        print_bst(*bst.left);
+//    }
+//    std::cout << "; right: ";
+//    if (!bst.right) std::cout << "nullptr ";
+//    else {
+//        std::cout << "\t\n";
+//        print_bst(*bst.right);
+//    }
+//    std::cout << std::endl;
+//}
 
